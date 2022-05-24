370548c948523ca3e9b5264d6af6f08bd72b8b02
Derric Lee <76984489+hysticali@users.noreply.github.com>
2022-05-23 17:20:11 -0700

    Cargo Build

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-a73972001433c395.d
@@ -0,0 +1,5 @@
+d:\Documents\GitHub\Hello-World\hello_cargo\target\rls\debug\deps\hello_cargo-a73972001433c395.rmeta: src\main.rs
+
+d:\Documents\GitHub\Hello-World\hello_cargo\target\rls\debug\deps\hello_cargo-a73972001433c395.d: src\main.rs
+
+src\main.rs:
