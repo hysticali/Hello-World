95b7c437c21c4244f2aaa73cc7c0747becc558bd
Derric Lee <76984489+hysticali@users.noreply.github.com>
2022-05-23 17:18:35 -0700

    File structure updates

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\Documents\GitHub\Hello-World\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+D:\Documents\GitHub\Hello-World\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\Documents\GitHub\Hello-World\hello_cargo\target\debug\hello_cargo.exe: D:\Documents\GitHub\Hello-World\hello_cargo\src\main.rs
